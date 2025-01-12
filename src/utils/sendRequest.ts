ad91fab88444827d805a1d12633b7ad386d66509
Valentan390 <valentynhavrylenko8886@gmail.com>
2025-01-12 22:11:06 +0200

    Update sendRequest.ts

--- src/utils/sendRequest.ts
+++ src/utils/sendRequest.ts
@@ -4,7 +4,7 @@ export const sendRequest = async () => {
   const localhost = process.env.Local_Host;
 
   try {
-    const { data } = await axios.get<string>(`http://${localhost}:${3000}/api`);
+    const { data } = await axios.get<string>(`http://${localhost}/api`);
     console.log('Server Response:', data);
   } catch (error) {
     console.error('Error making request:', error.message);
